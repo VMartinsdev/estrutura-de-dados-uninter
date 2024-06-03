6b565e735e9e11e18c8091532ac27a9a244136df
Vitor Martins <vitor.martins.dev@gmail.com>
2024-06-03 10:39:15 -0300

    Create Iterativo.py

---
+++ Metodo_recursio_e_iterativo/Iterativo.py
@@ -0,0 +1,12 @@
+def fatorial_iterativo(n)
+    fat = 1
+    if n < 0:
+         return None
+    elif n == 0 or n == 1:
+         return fat
+    else
+         for i in range(1, n+1):
+              fat *=1
+        return fat
+
+print(fatorial_iterativo(5))''
