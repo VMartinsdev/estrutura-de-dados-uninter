3a8a46878475f23e75a4846199d83864707dc1c0
Vitor Martins <vitor.martins.dev@gmail.com>
2024-06-04 09:11:06 -0300

    teste python

--- Metodo_recursio_e_iterativo/Iterativo.py
+++ Metodo_recursio_e_iterativo/Iterativo.py
@@ -1,12 +1,25 @@
-def fatorial_iterativo(n)
-    fat = 1
-    if n < 0:
-         return None
-    elif n == 0 or n == 1:
-         return fat
-    else
-         for i in range(1, n+1):
-              fat *=1
-        return fat
 
-print(fatorial_iterativo(5))''
+def fatorial_iterativo(n):
+  n = 5
+  fat = 1
+  if n <0:
+    return None
+  elif n == 0 or n == 1:
+    return fat
+  else:
+      for i in range(1, n+1):
+          fat *= i
+      return fat
+
+print(fatorial_iterativo(5))
+
+# Série Fibonacci
+n = 5
+fat = 1
+f1 = 1
+f2 = 1
+for i in range(1, n+1):
+   f3 = i+f1
+for u in range(1, n+1)
+     f4 = f3+f2
+print(f3)
